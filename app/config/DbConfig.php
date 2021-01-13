8791d66e4615b39d070dd94eb6f6341216b5a35f
kukivac <50522557+kukivac@users.noreply.github.com>
2021-01-13 16:10:41 +0100

    minor edits

--- app/config/DbConfig.php
+++ app/config/DbConfig.php
@@ -8,8 +8,15 @@ namespace app\config;
  */
 class DbConfig
 {
+
     public static string $host = '127.0.0.1';
     public static string $username = 'root';
     public static string $pass = '';
     public static string $database = 'database';
+    /*
+    public static string $host = 'c072um.forpsi.com:3306';
+    public static string $username = 'f135079';
+    public static string $pass = 'wLcXwL62';
+    public static string $database = 'f135079';
+    */
 }
--- www/index.php
+++ www/index.php
@@ -12,14 +12,12 @@ require("../vendor/autoload.php");
 function autoloadFunction($class)
 {
     $classname="./../" . str_replace("\\","/",$class) . ".php";
-    var_dump($classname);
     if (is_readable($classname)) {
         /** @noinspection PhpIncludeInspection */
         require($classname);
     }
 }
 spl_autoload_register("autoloadFunction");
-var_dump(phpversion());
 session_start();
 $router = new Router();
 $router->process(array($_SERVER['REQUEST_URI']));
